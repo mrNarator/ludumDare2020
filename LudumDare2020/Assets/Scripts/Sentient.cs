027e7706d4c56b4e68b26092aef3fa5fb2e52bd4
karolis_germanavicius <karolis.germanavicius@gmail.com>
2020-10-03 16:51:54 +0300

    - stats script

--- LudumDare2020/Assets/Scripts/Sentient.cs
+++ LudumDare2020/Assets/Scripts/Sentient.cs
@@ -5,23 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider))]
 public class Sentient : MonoBehaviour
 {
-    public float maxStrength = 5;
-    public float maxFood = 5;
-    public float maxDrink = 5;
-    public float maxLove = 5;
-
-
-    float Strength = 1;
-    float Food = 1;
-    float Drink = 1;
-    float Love = 0;
+    
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ LudumDare2020/Assets/Scripts/Stats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public float maxHandsome = 5;
+    public float maxStrength = 5;
+    public float maxFood = 5;
+    public float maxDrink = 5;
+    public float maxLove = 5;
+
+    float Handsome = 1;
+    float Strength = 1;
+    float Food = 1;
+    float Drink = 1;
+    float Love = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
